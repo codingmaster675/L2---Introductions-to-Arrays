2f846b25b321cf71c2ff5aa6023acb0d3336a568
codingmaster675 <topsecretshadowgovernment@gmail.com>
2024-09-07 14:52:58 +0300

    Arrays in JS

---
+++ script.js
@@ -0,0 +1,13 @@
+// Arrays is a collection of items
+
+var cities=["Dubai","Nairobi","London","Canberra","Washington D.C"]
+
+console.log(cities[3])
+document.getElementById("cities").innerHTML=cities[3]
+document.write(cities.length)
+document.write("<br>")
+document.write(cities.join("*"))
+document.write("<br>")
+document.write(cities.pop())
+document.write("<br>")
+document.write(cities)
